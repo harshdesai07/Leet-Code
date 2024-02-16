f8e2a987af55f49bcfe51e9e517ebbf61e93cb40
Harsh Desai <hdesai29072003@gmail.com>
2024-02-16 17:11:49 +0530

    16-02-2024

---
+++ leet code 2390/leetcode2390.java
@@ -0,0 +1,18 @@
+public class leetcode2390 {
+    public String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '*'){
+                sb.deleteCharAt(sb.length()-1);
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
