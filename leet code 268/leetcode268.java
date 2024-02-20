6b58236ff7d80b497fb72695ecd900c572fe05be
Harsh Desai <hdesai29072003@gmail.com>
2024-02-20 07:59:30 +0530

    20-02-2024

---
+++ leet code 268/leetcode268.java
@@ -0,0 +1,16 @@
+public class leetcode268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+
+        for(int i=0; i<=n; i++){
+            sum+=i;
+        }
+
+        for(int num: nums){
+            sum-=num;
+        }
+
+        return sum;
+    }
+}
