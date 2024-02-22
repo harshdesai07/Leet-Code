9cde77c45bf6d139f180a92d33b768f2cc2b2209
Harsh Desai <hdesai29072003@gmail.com>
2024-02-22 07:05:40 +0530

    22-02-2024

---
+++ leet code 263/leetcode263.java
@@ -0,0 +1,9 @@
+public class leetcode263 {
+    public boolean isUgly(int n) {
+        for(int i=2; i<=5 && n>0; i++){
+            while(n%i == 0) n/=i;
+        }
+
+        return n==1;
+    }
+}
