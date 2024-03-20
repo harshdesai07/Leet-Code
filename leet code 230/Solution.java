4935d1d1ab68aa5514b917cfa11af97cdbd04138
Harsh Desai <hdesai29072003@gmail.com>
2024-03-20 12:58:51 +0530

    leet code 230

---
+++ leet code 230/Solution.java
@@ -0,0 +1,46 @@
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class Solution {
+    // // inorder
+    // public void helper(TreeNode root, ArrayList<Integer> al) {
+    //     // base case
+    //     if(root == null) return;
+
+    //     helper(root.left, al);
+    //     al.add(root.val);
+    //     helper(root.right, al);
+    // }
+
+    // Method 2
+     // inorder
+     int count = 0;
+     int ans;
+    public void helper(TreeNode root, int k) {
+        // base case
+        if(root == null) return;
+
+        helper(root.left, k);
+        count++;
+        if(count == k){
+            ans = root.val;
+            return;
+        }
+        helper(root.right, k);
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+        return ans;
+    }
+}
