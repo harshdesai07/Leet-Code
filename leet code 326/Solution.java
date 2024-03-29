cb3f6c82cb6cdab673f40495c1ba8446e287db19
Harsh Desai <hdesai29072003@gmail.com>
2024-03-29 12:10:10 +0530

    29-03-2024

---
+++ leet code 326/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+        // if(n == 0) return false;
+        // if(n == 1) return true;
+        
+        // if(n % 3 != 0) return false;
+        
+        // return isPowerOfThree(n/3);   
+
+        while(n>=3){
+            if(n%3!=0) return false;
+            n/=3;
+        }
+        return n==1;
+    }
+}
